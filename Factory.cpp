137cd4c71aa3d05686a0d9c942d28af3cabdae4a
salomonhippie <salomonhippie@gmail.com>
2008-08-09 20:23:05 +0000

    now with skydome and moveable spheres

--- Factory.cpp
+++ Factory.cpp
@@ -15,6 +15,7 @@
 
 #include <Renderers/OpenGL/Renderer.h>
 #include <Renderers/OpenGL/RenderingView.h>
+#include <Renderers/OpenGL/TextureLoader.h>
 
 #include <Sound/OpenALSoundSystem.h>
 #include <Sound/ISoundSystem.h>
@@ -24,6 +25,8 @@
 
 #include <Resources/VorbisResource.h>
 #include <Resources/ISoundResource.h>
+#include <Resources/TGAResource.h>
+#include <Resources/ITextureResource.h>
 
 #include <Scene/SphereNode.h>
 
@@ -32,6 +35,11 @@
 
 #include <Utils/MoveHandler.h>
 #include <Utils/QuitHandler.h>
+#include <Utils/PropertyList.h>
+#include <Sky/SkyDome.h>
+
+#include <Core/IModule.h>
+
 
 #include <string>
 
@@ -44,30 +52,60 @@ using namespace OpenEngine::Sound;
 using namespace OpenEngine::Resources;
 using namespace OpenEngine::Utils;
 
+using OpenEngine::Sky::SkyDome;
 
-class PlayHandler : public IListener<KeyboardEventArg> {
-
+class Handler : public IListener<KeyboardEventArg>, public IModule {
 private:
-    SoundNode* sn;
+    TransformationNode*** spheres;
+    int type;
 public:
 
-    PlayHandler(SoundNode* sn): sn(sn){}
-    ~PlayHandler() {}
+    Handler(TransformationNode*** spheres): spheres(spheres), type(0){}
+    ~Handler() {}
     
     void Handle(KeyboardEventArg arg) {
         if (arg.type == KeyboardEventArg::PRESS) {
             switch (arg.sym) {
-            case KEY_p: sn->GetSound()->Play(); break;
-            case KEY_o: sn->GetSound()->Stop(); break;
-            case KEY_i: sn->GetSound()->Pause(); break;
-            case KEY_UP: sn->GetSound()->SetMaxDistance(sn->GetSound()->GetMaxDistance()+1); break;
-            case KEY_DOWN: sn->GetSound()->SetMaxDistance(sn->GetSound()->GetMaxDistance()-1); break;
+            case KEY_u: type = 1; break;
+            case KEY_i: type = 2; break;
+            case KEY_o: type = 3; break;
             default: 
                 break;
             }
         }
     }
     
+    void Initialize() {}
+    void Deinitialize() {}
+    bool IsTypeOf(const std::type_info& inf) { return inf == typeid(Handler);}
+    void Process(const float dt, const float percent) {
+        if (type == 0) return;
+        float dist;
+        switch (type) {
+        case 1: dist = 100; break;
+        case 2: dist = 500; break;
+        case 3: dist = 800; break;
+        }
+        
+        float currdist = (spheres[0][1]->GetPosition() - spheres[0][0]->GetPosition()).GetLength();
+        if (currdist < dist) {
+            currdist += 1*dt;
+        }
+        else currdist -= 1*dt;
+
+        if (max(currdist-dist,dist-currdist) < 10) {
+            currdist = dist;
+            type = 0;
+        }
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                spheres[i][j]->SetPosition(Vector<3,float>(i*currdist,0,j*currdist));
+            }
+        }
+    }
+
+
     void BindToEventSystem() {
         IKeyboard::keyEvent.Attach(*this);
     }
@@ -84,12 +122,13 @@ Factory::Factory() {
 
     // frustum hack
     Frustum* frustum = new Frustum(*camera);
-    frustum->SetFar(5000);
-    frustum->SetNear(0.0001);
+    frustum->SetFar(15000);
+    frustum->SetNear(1);
     viewport->SetViewingVolume(frustum);
 
     renderer = new Renderer();
     renderer->process.Attach(*(new RenderingView(*viewport)));
+    renderer->initialize.Attach(*(new TextureLoader()));
 }
 
 Factory::~Factory() {
@@ -100,93 +139,111 @@ Factory::~Factory() {
 }
 
 bool Factory::SetupEngine(IGameEngine& engine) {
-    try {
-        // Setup input handling
-        SDLInput* input = new SDLInput();
-        engine.AddModule(*input);
+    // Setup input handling
+    SDLInput* input = new SDLInput();
+    engine.AddModule(*input);
 
 
-        // Register the handler as a listener on up and down keyboard events.
-        MoveHandler* move_h = new MoveHandler(*camera);
-        engine.AddModule(*move_h);
-        move_h->BindToEventSystem();
-        QuitHandler* quit_h = new QuitHandler();
-        quit_h->BindToEventSystem();
+    // Register the handler as a listener on up and down keyboard events.
+    MoveHandler* move_h = new MoveHandler(*camera);
+    engine.AddModule(*move_h);
+    move_h->BindToEventSystem();
+    QuitHandler* quit_h = new QuitHandler();
+    quit_h->BindToEventSystem();
 
-        // Create scene root
-        SceneNode* root = new SceneNode();
-        this->renderer->SetSceneRoot(root);
+    // Create scene root
+    SceneNode* root = new SceneNode();
+    this->renderer->SetSceneRoot(root);
 
-        //First we set the resources directory
-        //         DirectoryManager::AppendPath("/");
-        DirectoryManager::AppendPath(""); //current directory
-        DirectoryManager::AppendPath("projects/SoundPark/data/GregoryTripi22/ogg");        
-        // load the resource plug-ins
-        ResourceManager<ISoundResource>::AddPlugin(new VorbisResourcePlugin());
+    //First we set the resources directory
+    //         DirectoryManager::AppendPath("/");
+    DirectoryManager::AppendPath(""); //current directory
+    DirectoryManager::AppendPath("projects/SoundPark/data/GregoryTripi22/ogg/");        
+    DirectoryManager::AppendPath("projects/SoundPark/data/Skydome/");        
+    // load the resource plug-ins
+    ResourceManager<ISoundResource>::AddPlugin(new VorbisResourcePlugin());
+    ResourceManager<ITextureResource>::AddPlugin(new TGAPlugin());
         
-        ISoundSystem* openalsmgr = new OpenALSoundSystem(root, camera);
-        engine.AddModule(*openalsmgr);
+    ISoundSystem* openalsmgr = new OpenALSoundSystem(root, camera);
+    engine.AddModule(*openalsmgr);
 
-        SoundRenderer* sr = new SoundRenderer();
-        renderer->preProcess.Attach(*sr);
-        string soundarray[3][3] = {{"Atmosphere Pad 01.ogg","Atmosphere Pad 02.ogg", "Atmosphere Pad 03.ogg"}, 
-                                      {"Atmosphere Pad 04.ogg", "Atmosphere Pad 05.ogg", "Atmosphere Pad 06.ogg"},
-                                      {"Atmosphere Pad 07.ogg", "Atmosphere Pad 08.ogg", "Atmosphere Pad 08.ogg"}};
+    SoundRenderer* sr = new SoundRenderer();
+    renderer->preProcess.Attach(*sr);
+    string soundarray[3][3] = {{"Atmosphere Pad 01.ogg","Atmosphere Pad 02.ogg", "Atmosphere Pad 03.ogg"}, 
+                               {"Atmosphere Pad 04.ogg", "Atmosphere Pad 05.ogg", "Atmosphere Pad 06.ogg"},
+                               {"Atmosphere Pad 07.ogg", "Atmosphere Pad 08.ogg", "Atmosphere Pad 08.ogg"}};
      
         
-        float dist = 100;
-        float soundrad = 50;
+    float dist = 500;
+    float soundrad = 350;
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                ISoundResourcePtr soundres = 
-                    ResourceManager<ISoundResource>::Create(string("projects/SoundPark/data/GregoryTripi22/ogg/")+soundarray[i][j]);
-                ISound* sound = openalsmgr->CreateSound(soundres);
-                sound->SetMaxDistance(soundrad*2);
-                sound->SetLooping(true);
-                sound->SetGain(10.0);
-                sound->SetMaxGain(20.0);
-                sound->SetMinGain(0.0);
-                sound->SetRolloffFactor(0.5);
-                //sound->SetReferenceDistance(soundrad);
-                // float g = sound->GetGain();
-//                 logger.info << "g: " << g <<logger.end;
-                SoundNode* soundNode = new SoundNode(sound);
-                sound->Play();
+    TransformationNode*** spheres = new TransformationNode**[3];
+    for (int i = 0; i < 3; i++)
+        spheres[i] = new TransformationNode*[3];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            ISoundResourcePtr soundres = 
+                ResourceManager<ISoundResource>::Create(soundarray[i][j]);
+            ISound* sound = openalsmgr->CreateSound(soundres);
+            sound->SetMaxDistance(soundrad);
+            sound->SetLooping(true);
+            sound->SetGain(10.0);
+            //sound->SetMaxGain(20.0);
+            sound->SetMinGain(0.0);
+            sound->SetRolloffFactor(1);
+            //sound->SetReferenceDistance(soundrad);
+            // float g = sound->GetGain();
+            //                 logger.info << "g: " << g <<logger.end;
+            SoundNode* soundNode = new SoundNode(sound);
+            sound->Play();
                 
 
-                TransformationNode* tn = new TransformationNode();
-                tn->Move(i*dist*2,0,j*dist*2);
-                Vector<3,float> col;
-                if ((i+j) %2 == 0)
-                    col = Vector<3,float>(1.0,0.2,0.2);
-                else col = Vector<3,float>(0.2,1.0,0.2);
-                sr->AddSoundNode(soundNode,col);
-                tn->AddNode(soundNode);
-                root->AddNode(tn);
-            }
+            TransformationNode* tn = new TransformationNode();
+            spheres[i][j] = tn;
+            tn->Move(i*dist,0,j*dist);
+            Vector<3,float> col;
+            if ((i+j) %2 == 0)
+                col = Vector<3,float>(1.0,0.2,0.2);
+            else col = Vector<3,float>(0.2,1.0,0.2);
+            sr->AddSoundNode(soundNode,col);
+            tn->AddNode(soundNode);
+            root->AddNode(tn);
         }
-
-        // lighting setup
-        TransformationNode* tn2 = new TransformationNode();
-        tn2->Move(0,0,-100);
-        PointLightNode* dln = new PointLightNode();
-        dln->linearAtt = 0.001;
-        dln->quadAtt = 0.0001;
-        dln->constAtt = 0.5;
-        tn2->AddNode(dln);
-        root->AddNode(tn2);
-        SphereNode* sphere2 = new SphereNode();        
-        dln->AddNode(sphere2);
-
-
-
-        //        engine.AddModule(*(new Statistics(1000)));
-
-    } catch (const Exception& ex) {
-        logger.error << "An exception occurred: " << ex.what() << logger.end;
-        throw ex;
     }
+
+    // load skydome
+    string skyfile = "skydome.txt";
+    PropertyList* skylist = new PropertyList(skyfile);
+    SkyDome* sky = new SkyDome(skylist);
+    root->AddNode(sky->GetSceneNode());
+    
+    Handler* h = new Handler(spheres);
+    h->BindToEventSystem();
+    engine.AddModule(*h);
+    // lighting setup
+    TransformationNode* tn2 = new TransformationNode();
+    tn2->Move(0,100,0);
+    PointLightNode* dln = new PointLightNode();
+    //dln->linearAtt = 0.01;
+    //dln->quadAtt = 0.0001;
+    //dln->constAtt = 0.5;
+ //    dln->specular = Vector<4,float>(1.0,1.0,1.0,1.0);
+//     dln->diffuse = Vector<4,float>(1.0,1.0,1.0,1.0);
+//     dln->ambient = Vector<4,float>(0.0,0.0,0.0,1.0);
+    tn2->AddNode(dln);
+    root->AddNode(tn2);
+    SphereNode* sphere2 = new SphereNode();        
+    dln->AddNode(sphere2);
+
+
+
+    //        engine.AddModule(*(new Statistics(1000)));
+
+//     } catch (const Exception& ex) {
+//         logger.error << "An exception occurred: " << ex.what() << logger.end;
+//         throw ex;
+//     }
     // Return true to signal success.
     return true;
 }
